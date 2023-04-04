61f75792fca55f561c3bea3883c1c154f65aaafa
Pedro Ramos <ramuspedro@gmail.com>
2023-04-03 21:28:00 -0300

    add: git project

---
+++ src/main/kotlin/com/example/bank/HelloWorldController.kt
@@ -0,0 +1,13 @@
+package com.example.bank
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("api/hello")
+class HelloWorldController {
+
+    @GetMapping
+    fun helloWord(): String = "Hello, this is a REST endpoint!"
+}
